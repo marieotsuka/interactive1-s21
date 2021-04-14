be6b5c4efd3e80f4fba25717d8b10212c905bf2f
Marie Otsuka <motsuka.gd@gmail.com>
2021-04-14 10:59:15 -0400

    week 12 materials, resources update

--- site/templates/projects.php
+++ site/templates/projects.php
@@ -20,13 +20,14 @@
 
 
     <h1 id="ex-title">Exercises</h1>
-    <p>You should have the following links from your class website:
+    <p>Some of the exeercises have to do with working on your class site itself. For others, you should have a link to the exercise results from your class website, listed below. 
     </p>
     <ul>
       <li>Collective Alphabet</li>
       <li>Figma Sketches for Project 1</li>
       <li>Inspector Exercise Screenshot</li>
       <li>CSS Still Life with :hover state</li>
+      <li>Poster Exercise</li>
     </ul>
     <ul class="exercises">
     <?php foreach($page->children()->visible()->filterBy('project', '0')->sortBy('title') as $text): ?>
@@ -38,6 +39,10 @@
     <?php endforeach ?>
 
     </ul>
+
+    <h1 id="ex-title">Codecademy</h1>
+    <p>On your class site, please also provide a link to your <a href="https://www.codecademy.com">Codecademy</a> Badges. To find your Badges URL, log into Codecademy, then click on your icon to go to <strong>Profile > Badges</strong>
+    </p>
 
   </main>
 
